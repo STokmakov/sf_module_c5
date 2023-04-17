bb8eb8a9354e7fc97cd5d7100eef2b75d44ba635
STokmakov <Sergey_Tokmakov@list.ru>
2023-04-17 12:24:10 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-KEYCONVERT = "8d986dd171e6a3bbdaca034e"                   # ключ для подключения к API exchangerate-api.com
-TOKEN = "5856107999:AAHH0x6pnub6XMOBfc04KSt_CM3BngBGPUg"  # токен для подключения к боту SF_c5_mybot
-exchanges = {                                             # допустимые значения валют
+KEYCONVERT = "Тут_ключ"                   # ключ для подключения к API exchangerate-api.com
+TOKEN = "Тут_токен"                       # токен для подключения к боту
+exchanges = {                             # допустимые значения валют
     'доллар': 'USD',
     'евро': 'EUR',
     'рубль': 'RUB'
